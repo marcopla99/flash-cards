69275b27ca2ee710b7759af0e5c4b09b8a593145
Marco <plasmati@d-tt.nl>
2022-11-12 10:18:07 +0100

    Center text for empty data

--- app/src/main/java/com/marcopla/flashcards/MainActivity.kt
+++ app/src/main/java/com/marcopla/flashcards/MainActivity.kt
@@ -3,9 +3,12 @@ package com.marcopla.flashcards
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import com.marcopla.flashcards.data.FlashCard
 import com.marcopla.flashcards.ui.theme.FlashCardsTheme
 
@@ -23,7 +26,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ContentSection(flashCards: List<FlashCard>) {
     if (flashCards.isEmpty()) {
-        Text(text = stringResource(id = R.string.noFlashCardsCreated))
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(id = R.string.noFlashCardsCreated),
+            textAlign = TextAlign.Center
+        )
     } else {
         TODO()
     }
