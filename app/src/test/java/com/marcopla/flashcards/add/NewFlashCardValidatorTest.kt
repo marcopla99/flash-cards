f47f34d3f1774ab6ffea2553148e57f9fbb9d492
Marco <plasmati.marco@gmail.com>
2022-11-20 18:20:23 +0100

    Introduce a task execution extension for LiveData

--- app/src/test/java/com/marcopla/flashcards/add/NewFlashCardValidatorTest.kt
+++ app/src/test/java/com/marcopla/flashcards/add/NewFlashCardValidatorTest.kt
@@ -1,8 +1,11 @@
 package com.marcopla.flashcards.add
 
+import com.marcopla.flashcards.InstantTaskExecutorExtension
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(InstantTaskExecutorExtension::class)
 class NewFlashCardValidatorTest {
 
     @Test
