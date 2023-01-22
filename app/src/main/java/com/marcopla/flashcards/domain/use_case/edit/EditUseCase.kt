1ac4fd79100a228fa80689f95afcddc7c4758423
marcopla99 <plasmati.marco@gmail.com>
2023-01-22 12:58:09 +0100

    Throw exception when frontText is invalid

--- app/src/main/java/com/marcopla/flashcards/domain/use_case/edit/EditUseCase.kt
+++ app/src/main/java/com/marcopla/flashcards/domain/use_case/edit/EditUseCase.kt
@@ -3,10 +3,14 @@ package com.marcopla.flashcards.domain.use_case.edit
 import com.marcopla.flashcards.data.model.FlashCard
 import com.marcopla.flashcards.data.repository.DuplicateInsertionException
 import com.marcopla.flashcards.data.repository.FlashCardRepository
+import com.marcopla.flashcards.domain.use_case.add.InvalidFrontException
 
 class EditUseCase(private val flashCardRepository: FlashCardRepository) {
     @Throws(DuplicateInsertionException::class)
     suspend fun invoke(frontText: String, backText: String) {
+        if (frontText.isBlank()) {
+            throw InvalidFrontException()
+        }
         flashCardRepository.add(FlashCard(frontText = frontText, backText = backText))
     }
 }
---
+++ app/src/test/java/com/marcopla/flashcards/presentation/edit/EditUseCaseTest.kt
@@ -0,0 +1,23 @@
+package com.marcopla.flashcards.presentation.edit
+
+import com.marcopla.flashcards.domain.use_case.add.InvalidFrontException
+import com.marcopla.flashcards.domain.use_case.edit.EditUseCase
+import com.marcopla.testing.TestFlashCardRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class EditUseCaseTest {
+
+    @Test
+    fun frontText_whenIsEmpty_thenThrowException() = runTest {
+        val useCase = EditUseCase(TestFlashCardRepository())
+
+        assertThrows(InvalidFrontException::class.java) {
+            runBlocking { useCase.invoke("", ":backText:") }
+        }
+    }
+}
