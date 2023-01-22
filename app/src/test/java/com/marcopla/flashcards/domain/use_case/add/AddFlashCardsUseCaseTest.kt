cfd1726c3335485b43b00b9b880a47313f28c577
marcopla99 <plasmati.marco@gmail.com>
2023-01-22 18:19:06 +0100

    Move AddFlashCardsUseCaseTest.kt to domain package

--- app/src/test/java/com/marcopla/flashcards/domain/use_case/add/AddFlashCardsUseCaseTest.kt
+++ app/src/test/java/com/marcopla/flashcards/domain/use_case/add/AddFlashCardsUseCaseTest.kt
@@ -1,15 +1,14 @@
-package com.marcopla.flashcards.presentation.add
+package com.marcopla.flashcards.domain.use_case.add
 
 import com.marcopla.flashcards.MainDispatcherExtension
 import com.marcopla.flashcards.data.data_source.FakeFlashCardDao
 import com.marcopla.flashcards.data.repository.FlashCardRepositoryImpl
-import com.marcopla.flashcards.domain.use_case.add.AddFlashCardsUseCase
 import com.marcopla.flashcards.presentation.screen.add.AddViewModel
 import com.marcopla.flashcards.presentation.screen.add.BackTextState
 import com.marcopla.flashcards.presentation.screen.add.FrontTextState
-import kotlinx.coroutines.*
-import kotlinx.coroutines.test.*
-import org.junit.jupiter.api.Assertions.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -26,7 +25,7 @@ class AddFlashCardsUseCaseTest {
 
         viewModel.attemptSubmit(blankFrontText, ":backText:")
 
-        assertEquals(
+        Assertions.assertEquals(
             FrontTextState("", showError = true),
             viewModel.frontTextState.value,
         )
@@ -40,7 +39,7 @@ class AddFlashCardsUseCaseTest {
 
         viewModel.attemptSubmit(":frontText:", blankBackText)
 
-        assertEquals(
+        Assertions.assertEquals(
             BackTextState("", showError = true),
             viewModel.backTextState.value
         )
