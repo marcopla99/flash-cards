802c0cd27bf924d6783595a7a1e5c2895da8f753
marcopla99 <plasmati.marco@gmail.com>
2023-01-29 17:55:13 +0100

    Test AddViewModel state for blank front text

---
+++ app/src/test/java/com/marcopla/flashcards/presentation/add/AddViewModelTest.kt
@@ -0,0 +1,27 @@
+package com.marcopla.flashcards.presentation.add
+
+import com.marcopla.flashcards.MainDispatcherExtension
+import com.marcopla.flashcards.domain.use_case.add.AddFlashCardsUseCase
+import com.marcopla.flashcards.presentation.screen.add.AddViewModel
+import com.marcopla.flashcards.presentation.screen.add.FrontTextState
+import com.marcopla.testing.TestFlashCardRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@ExtendWith(MainDispatcherExtension::class)
+class AddViewModelTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", " ", "  "])
+    fun frontText_whenIsBlank_thenShowError(blankFrontText: String) {
+        val viewModel = AddViewModel(AddFlashCardsUseCase(TestFlashCardRepository()))
+
+        viewModel.attemptSubmit(blankFrontText, ":backText:")
+
+        assertEquals(FrontTextState(showError = true), viewModel.frontTextState.value)
+    }
+}
