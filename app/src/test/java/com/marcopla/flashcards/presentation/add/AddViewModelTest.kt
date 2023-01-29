6f081efcac45f6a3db0b2ab4f7ea0922e2f77e5f
marcopla99 <plasmati.marco@gmail.com>
2023-01-29 17:57:23 +0100

    Test AddViewModel state for blank back text

--- app/src/test/java/com/marcopla/flashcards/presentation/add/AddViewModelTest.kt
+++ app/src/test/java/com/marcopla/flashcards/presentation/add/AddViewModelTest.kt
@@ -3,6 +3,7 @@ package com.marcopla.flashcards.presentation.add
 import com.marcopla.flashcards.MainDispatcherExtension
 import com.marcopla.flashcards.domain.use_case.add.AddFlashCardsUseCase
 import com.marcopla.flashcards.presentation.screen.add.AddViewModel
+import com.marcopla.flashcards.presentation.screen.add.BackTextState
 import com.marcopla.flashcards.presentation.screen.add.FrontTextState
 import com.marcopla.testing.TestFlashCardRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -23,5 +24,15 @@ class AddViewModelTest {
         viewModel.attemptSubmit(blankFrontText, ":backText:")
 
         assertEquals(FrontTextState(showError = true), viewModel.frontTextState.value)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", " ", "  "])
+    fun backText_whenIsBlank_thenShowError(blankBackText: String) {
+        val viewModel = AddViewModel(AddFlashCardsUseCase(TestFlashCardRepository()))
+
+        viewModel.attemptSubmit(":frontText:", blankBackText)
+
+        assertEquals(BackTextState(showError = true), viewModel.backTextState.value)
     }
 }
