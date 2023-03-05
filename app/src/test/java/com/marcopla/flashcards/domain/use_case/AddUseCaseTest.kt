0a74c3de77c8c25244d715acba89f700316dd3f4
marcopla99 <plasmati.marco@gmail.com>
2023-03-05 15:58:39 +0100

    Replace unit under test as described by the class name

--- app/src/test/java/com/marcopla/flashcards/domain/use_case/AddUseCaseTest.kt
+++ app/src/test/java/com/marcopla/flashcards/domain/use_case/AddUseCaseTest.kt
@@ -2,13 +2,13 @@ package com.marcopla.flashcards.domain.use_case
 
 import com.marcopla.flashcards.MainDispatcherExtension
 import com.marcopla.flashcards.data.repository.FlashCardRepositoryImpl
-import com.marcopla.flashcards.presentation.screen.add.AddViewModel
-import com.marcopla.flashcards.presentation.screen.add.BackTextState
-import com.marcopla.flashcards.presentation.screen.add.FrontTextState
+import com.marcopla.flashcards.domain.use_case.exceptions.InvalidBackTextException
+import com.marcopla.flashcards.domain.use_case.exceptions.InvalidFrontTextException
 import com.marcopla.testing_shared.FakeFlashCardDao
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -19,29 +19,29 @@ class AddUseCaseTest {
 
     @ParameterizedTest
     @ValueSource(strings = ["", " ", "  "])
-    fun frontText_whenIsBlank_thenReturnInvalidState(blankFrontText: String) = runTest {
-        val viewModel =
-            AddViewModel(AddUseCase(FlashCardRepositoryImpl(FakeFlashCardDao())))
+    fun frontText_whenIsBlank_thenThrowException(blankFrontText: String) = runTest {
+        val addUseCase = AddUseCase(FlashCardRepositoryImpl(FakeFlashCardDao()))
 
-        viewModel.attemptSubmit(blankFrontText, ":backText:")
-
-        Assertions.assertEquals(
-            FrontTextState("", showError = true),
-            viewModel.frontTextState.value,
-        )
+        assertThrows(
+            InvalidFrontTextException::class.java
+        ) {
+            runBlocking {
+                addUseCase.invoke(blankFrontText, ":backText:")
+            }
+        }
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["", " ", "  "])
-    fun backText_whenIsBlank_thenReturnInvalidState(blankBackText: String) = runTest {
-        val viewModel =
-            AddViewModel(AddUseCase(FlashCardRepositoryImpl(FakeFlashCardDao())))
+    fun backText_whenIsBlank_thenThrowException(blankBackText: String) = runTest {
+        val addUseCase = AddUseCase(FlashCardRepositoryImpl(FakeFlashCardDao()))
 
-        viewModel.attemptSubmit(":frontText:", blankBackText)
-
-        Assertions.assertEquals(
-            BackTextState("", showError = true),
-            viewModel.backTextState.value
-        )
+        assertThrows(
+            InvalidBackTextException::class.java,
+        ) {
+            runBlocking {
+                addUseCase.invoke(":frontText:", blankBackText)
+            }
+        }
     }
 }
