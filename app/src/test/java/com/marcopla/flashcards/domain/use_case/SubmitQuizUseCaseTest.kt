44b40a723d0b40ed90024788df5dffcf6345adf5
marcopla99 <plasmati.marco@gmail.com>
2023-03-25 16:53:25 +0100

    Return correct validation and save correct result with correct guess

--- app/src/test/java/com/marcopla/flashcards/domain/use_case/SubmitQuizUseCaseTest.kt
+++ app/src/test/java/com/marcopla/flashcards/domain/use_case/SubmitQuizUseCaseTest.kt
@@ -3,6 +3,7 @@ package com.marcopla.flashcards.domain.use_case
 import com.marcopla.flashcards.data.model.FlashCard
 import com.marcopla.testing_shared.TestFlashCardRepository
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -10,7 +11,7 @@ import org.junit.jupiter.params.provider.ValueSource
 class SubmitQuizUseCaseTest {
     @ParameterizedTest
     @ValueSource(strings = ["", " ", "  "])
-    fun whenSubmittingBlankGuess_thenReturnWrong(blankGuess: String) {
+    fun whenSubmittingBlankGuess_thenAddWrongResult_andReturnWrongValidation(blankGuess: String) {
         val repository = TestFlashCardRepository()
         val submitQuizUseCase = SubmitQuizUseCase(repository)
 
@@ -24,7 +25,7 @@ class SubmitQuizUseCaseTest {
     }
 
     @Test
-    fun whenSubmittingWrongGuess_thenReturnWrong() {
+    fun whenSubmittingWrongGuess_thenAddWrongResult_andReturnWrongValidation() {
         val repository = TestFlashCardRepository()
         val submitQuizUseCase = SubmitQuizUseCase(repository)
 
@@ -35,5 +36,19 @@ class SubmitQuizUseCaseTest {
         val quizResult = repository.getCurrentResults().first()
         assertFalse(quizResult.isCorrect)
         assertFalse(isValidationCorrect)
+    }
+
+    @Test
+    fun whenSubmittingCorrectGuess_thenAddCorrectResult_andReturnCorrectValidation() {
+        val repository = TestFlashCardRepository()
+        val submitQuizUseCase = SubmitQuizUseCase(repository)
+
+        val isValidationCorrect = submitQuizUseCase.invoke(
+            FlashCard("Engels", "English"), "English"
+        )
+
+        val quizResult = repository.getCurrentResults().first()
+        assertTrue(quizResult.isCorrect)
+        assertTrue(isValidationCorrect)
     }
 }
